46b6533bb5b1fbd32e7e9e3ee3654f679b28d8aa
Tochukwu <tochukwusage4@gmail.com>
2020-06-02 18:19:09 +0100

    newest

--- application.py
+++ application.py
@@ -4,19 +4,23 @@ from flask import Flask, session, render_template, redirect, url_for, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask_wtf import Form
-from wtforms import TextField, BooleanField, PasswordField, TextAreaField, SubmitField, validators #Form
-#from fields import *
+from flask_wtf import Form, FlaskForm
+from wtforms import TextField, BooleanField, PasswordField, TextAreaField, SubmitField, validators, StringField #Form
 from login import *
-from wtforms.validators import InputRequired, Length, EqualTo
+from login import User
+from login import db
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+import psycopg2
+from psycopg2 import connect
 DEBUG = True
-import psycopg2 
+FLASK_DEBUG=1
+
 
 app = Flask(__name__)
-app.secret_key = 'replace_later'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ibryriynppxdtm:1275b6e1b940231cd521c3bbeab604da37c50304f8e429cc1b24fc4f2b3d7f58@ec2-54-225-227-125.compute-1.amazonaws.com:5432/d1brpuvqqbfnk1'
+app.secret_key = 'replace later'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://xkirkgsdjndrtd:04923e2fd7f601d40372a5aaef449d4b76fd601fd6171926c73b5421bc9ce23b@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d64djukmaep4ov'
 db = SQLAlchemy(app)
-
+app.debug = True
 
 
 API_key = 'HMOBPhAT2PnNTFaV4BiqEw'
@@ -30,38 +34,85 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
+def invalid_credentials(form, field):
+    """Username and password checker"""
+    username_entered = form.username.data
+    password_entered = field.data
 
+    # Check if credentials is valid
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("One of the details is incorrect")
 
+    elif password_entered != user_object.password:
+        raise ValidationError("One of the details is incorrect")
 
 
 
 
 class RegistrationForm(Form):
-    username = TextField('Username', validators=[InputRequired(message="Username required"), Length(min=4, max=20,
+
+    username = StringField('Username', validators=[InputRequired(message="Username required"), Length(min=4, max=20,
                                                                                message="Username between 4 and 25 characters")])
-    #email = TextField('Email Address', [validators.Length(min=6, max=50)])
+    email = TextField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', validators=[InputRequired(message="Use required"), Length(min=4, max=20,
                                                                                message="Username between 4 and 25 characters")])
     confirm = PasswordField('Repeat Password', validators=[InputRequired(message="Password required"),
                                                            EqualTo('password', message="Passwords must match")])
-    #accept_tos = BooleanField('I accept the <a href="/tos/">Terms of Service</a> and the <a href="/privacy/">Privacy Policy</a>',
+    #accept_tos = BooleanField('I accept the <a href="/tos/">Terms of
+    #Service</a> and the <a href="/privacy/">Privacy Policy</a>',
                              #[validators.Required()])
 
     submit_button = SubmitField('Create')
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists, please use a different username")
+    
+    def validate_email(self, email):
+        user_object = User.query.filter_by(email=email.data).first()
+        if user_object:
+            raise ValidationError("Email already exists, please use a different email")
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
     reg_form = RegistrationForm()
+    # updates database on successful registration
     if reg_form.validate_on_submit():
-        return "Success!"
+        username = reg_form.username.data
+        password = reg_form.password.data
+        email = reg_form.email.data
 
+        #adding user to DB
+        user = User(username=username, password=password, email=email)
+        db.session.add(user)
+        db.session.commit()            
+
+        return redirect(url_for('signin'))
     return render_template('login.html', form=reg_form)
 
 
+class LoginForm(FlaskForm):
+    """Login form"""
+    username = StringField('username', validators=[InputRequired(message="Username Required")])
+    password = PasswordField('password', validators=[InputRequired(message="Password required"), invalid_credentials])
+    submit_button = SubmitField('Create')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+
+    login_form = LoginForm()
+    
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        return "Successfully logged in"
+
+    return render_template('sign_in.html', form=login_form)
 
 
 
@@ -80,56 +131,6 @@ def index():
 
 
 
-
-
-
-
-
-
-
-
-#@app.route('/register/', methods=["GET", "POST"])
-#def register_page():
-#    try:
-#        form = RegistrationForm(request.form)
-
-#        if request.method == "POST" and form.validate():
-#            username = form.username.data
-#            email = form.email.data
-#            password = sha256.crypt.encrypt((str(form.password.data)))
-#            c, conn = connection()
-#            x = c.execute("SELECT * FROM users WHERE username = (%s)",
-#                          (thwart(username)))
-#            if int(len(x)) > 0:
-#                flash("This username is already taken, please choose another")
-#                return render_template('register.html', form=form)
-#            else:
-#                c.execute("INSERT INTO users (username, password, email, tracking) VALUES (%s, %s, %s, %s)",
-#                          (thwart(username), thwart(password), thwart(email), thwart
-
-
-
-
-#@app.route('/')
-#def home():
-#    """renders a sample page."""
-#    return "hello world!"
-
-#@app.route('/welcome')
-#def welcome():
-#    return render_template('welcome.html') #render template
-
-
-
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-#    error = None
-#    if request.method == 'POST':
-#        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-#            error = 'Invalid credentials. Please try again'
-#        else:
-#            return redirect(url_for('home'))
-#    return render_template('login.html', error=error)
 
 
 
--- login.py
+++ login.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import Model
+from flask_migrate import Migrate
 
 
 app =  Flask(__name__)
@@ -10,7 +11,15 @@ class User(db.Model):
     """User model"""
 
     __tablename__ = "users"
-
+   
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+      
